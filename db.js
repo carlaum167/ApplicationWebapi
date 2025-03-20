c3552bf7e08fe1336923151a387b5369a38058f6
carlaum167 <carlosgado167@gmail.com>
2025-03-20 11:33:42 -0300

    created connection function with mysql

---
+++ db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+
+async function connect(){
+    if(global.connection && global.connection.state !== 'disconnected') return global.connection;
+    const connectionString = 'mysql://root:root@localhost:3306/crud';
+    const connection = await mysql.createConnection(connectionString);
+    console.log('connected in MySQL!');
+    global.connection = connection;
+    return global.connection;
+}
+connect();
+
+module.exports = {}
