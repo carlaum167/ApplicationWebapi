4e857fbc725ab0705da0f252ad1e2292367a6eeb
carlaum167 <carlosgado167@gmail.com>
2025-03-20 11:52:47 -0300

    added express usage settings and request routing functionality, and server launcher -> app.js

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Is working!'}));
+app.use('/', router);
+
+app.listen(port);
+console.log('API working!');
